10288951fb69dd2001dbd0a02dd3bcff872ac323
stadnitska <132070892+stadnitska@users.noreply.github.com>
2025-10-21 22:46:54 +0300

    Add files via upload

---
+++ lib/components/Todos.svelte
@@ -0,0 +1,12 @@
+<script>
+  import TodoForm from "$lib/components/TodoForm.svelte";
+  import TodoList from "$lib/components/TodoList.svelte";
+</script>
+
+<h1>Todos</h1>
+
+<h2>Add Todo</h2>
+<TodoForm />
+
+<h2>Existing Todos</h2>
+<TodoList />
---
+++ routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+  import Todos from "$lib/components/Todos.svelte";
+</script>
+
+<Todos />
